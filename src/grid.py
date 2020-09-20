840032c12410e9a036b8e18afb3a6e8ae108b906
dprebyl <drakeprebyl@gmail.com>
2020-09-20 00:19:20 -0500

    Simplify win checking code

--- src/grid.py
+++ src/grid.py
@@ -22,46 +22,25 @@ class gridWrapper:
                 self.grid[y][x] = "miss"
                 #print("You Missed")
 
-    def __winner__ (self,x):
+    def __winner__ (self,win_ships):
         p1_count = 0
         p2_count = 0
-        win_count = x
-        for i in range(1,10):
-            for j in range(1,10):
-                if self.grid[j][i]=="hit":
+        for x in range(1,10):
+            for y in range(1,10):
+                if self.grid[y][x] == "hit":
                     p1_count += 1
-        for p in range(11,20):
-            for q in range(0,10):
-                if self.grid[q][p]=="hit":
+        for x in range(11,20):
+            for y in range(0,10):
+                if self.grid[y][x] == "hit":
                     p2_count += 1
         
-        if p1_count==5 or p1_count==4 or p1_count==2:
-            p1_count=0
+        # Determine the total number of spaces the ships required to win would occupy
+        win_spaces = win_ships * (win_ships+1) // 2
 
-        if p1_count==3:
-            p1_count=2
-        if p1_count==6:
-            p1_count=3
-        if p1_count==10:
-            p1_count=4
-        if p1_count==15:
-            p1_count=5  
-            
-        if p2_count==5 or p2_count==4 or p2_count==2:
-            p2_count=0
-        if p2_count==3:
-            p2_count=2
-        if p2_count==6:
-            p2_count=3
-        if p2_count==10:
-            p2_count=4
-        if p2_count==15:
-            p2_count=5
-
-        if p1_count == win_count:
+        if p1_count == win_spaces:
             print("\n==============\nPlayer 1 wins!\n==============\n")
             return True
         
-        if p2_count == win_count:
+        if p2_count == win_spaces:
             print("\n==============\nPlayer 2 wins!\n==============\n")
             return True
